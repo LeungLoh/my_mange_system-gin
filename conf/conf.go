5824ec54f27f61736ca180d4302b57bd74481afd
LeungLoh <843314152@qq.com>
2021-06-08 11:47:19 +0800

    add init

--- conf/conf.go
+++ conf/conf.go
@@ -1,5 +1,16 @@
 package conf
 
-func Init()  {
+import (
+	"my_mange_system/model"
+	"my_mange_system/util"
+	"os"
+)
 
+func Init() {
+
+	// 设置日志级别
+	util.BuildLogger(os.Getenv("LOG_LEVEL"))
+
+	// 连接数据库
+	model.Database(os.Getenv("MYSQL_DSN"))
 }
