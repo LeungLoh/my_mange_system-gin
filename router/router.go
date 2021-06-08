f33ed73c0b339c1488dd6ed1178cbe0e68733c44
LeungLoh <843314152@qq.com>
2021-06-08 16:25:26 +0800

    register router

--- router/router.go
+++ router/router.go
@@ -1,8 +1,20 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"my_mange_system/api"
 
-func NewRouter() *gin.Engine{
-	r:=gin.Default()
+	"github.com/gin-gonic/gin"
+)
+
+func NewRouter() *gin.Engine {
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	{
+		v1.POST("user/register", api.UserRegister)
+		v1.POST("user/login", api.UserLogin)
+		v1.POST("user/logout", api.UserLogout)
+		v1.GET("user/list", api.UserList)
+		v1.DELETE("user/delete", api.UserDelete)
+	}
 	return r
 }
