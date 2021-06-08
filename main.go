85e5ddf38d6b24593eafa5227964acae97a99fd9
LeungLoh <843314152@qq.com>
2021-06-08 16:26:58 +0800

    add global info

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"my_mange_system/conf"
 	"my_mange_system/router"
 )
 
 func main() {
-	r := router.NewRouter()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8000") // 监听并在 0.0.0.0:8080 上启动服务
+	conf.Init()
+	app := router.NewRouter()
+	app.Run(":8000") // 监听并在 0.0.0.0:8080 上启动服务
 }
